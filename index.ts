e866c5d9a9103db803c8352f90c140ac72a3baab
Mertenator <merten@gmail.com>
2024-03-18 21:01:35 +0200

    ignore warnings not in scope of assignment

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from 'express';
 import { bmiCalculator } from './bmiModule';
 const app = express();
